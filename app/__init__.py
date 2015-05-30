b60d9d6cbeeed6fd7a98eb1841b6929a9a5ac2c9
tomanizer <thomas.haederle@mailwitch.com>
2015-05-30 23:58:30 +0100

    added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import config
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
-    Bootstrap(app)
+    #Bootstrap(app)
     app.config.from_object(config[config_name])
+    bootstrap.init_app(app)
+    db.init_app(app)
     
     from .pypivot import pypivot as pypivot_blueprint
     app.register_blueprint(pypivot_blueprint)
--- config.py
+++ config.py
@@ -3,15 +3,19 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     
+
+basedir = os.path.abspath(os.path.dirname(__file__))    
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'tgtlse99'
+    SQLALCHEMY_DATA_URL = os.environ.get('DEV_DATABASE_RUL') or 'sqlite:////' + os.path.join(basedir, 'data-dev.sqlite')
     
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATA_URL = os.environ.get('TEST_DATABASE_RUL') or 'sqlite:////' + os.path.join(basedir, 'data-test.sqlite')
     
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATA_URL = os.environ.get('DATABASE_RUL') or 'sqlite:////' + os.path.join(basedir, 'data.sqlite')
 
 config = {
     'development': DevelopmentConfig,
