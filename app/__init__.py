9e7f6317808df3f8b85aee025d3ee2cacb235ae3
tomanizer <thomas.haederle@mailwitch.com>
2015-06-01 09:13:17 +0100

    setup professional structure

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from config import config
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,6 +15,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     
     from .pypivot import pypivot as pypivot_blueprint
     app.register_blueprint(pypivot_blueprint)
